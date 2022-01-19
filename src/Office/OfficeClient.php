319e3b53c8bb075a732c6a513f0da776f2ff1228
Menno van Leeuwen <menno@vleeuwen.me>
2022-01-19 17:00:28 +0100

    Removed double initialization of tenant in OfficeClient
    Signed-off-by: Menno van Leeuwen <menno@vleeuwen.me>

--- src/Office/OfficeClient.php
+++ src/Office/OfficeClient.php
@@ -51,7 +51,6 @@ final class OfficeClient
         $this->customer = new Customer($webApiClient, $this->router);
         $this->tenant = new Tenant($webApiClient, $this->router);
         $this->order = new Order($webApiClient, $this->router);
-        $this->tenant = new Tenant($webApiClient, $this->router);
         $this->contact = new Contact(new Agreement($webApiClient, $this->router));
 
         $this->subjects = new Subjects();
