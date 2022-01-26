cbd25d905a30f9993b28b28a94a3f8ce57a0ccf7
Daan Barbiers <daan@sandwave.io>
2022-01-26 16:08:05 +0100

    .

--- tests/Integration/Webhook/CustomerTest.php
+++ tests/Integration/Webhook/CustomerTest.php
@@ -16,48 +16,32 @@ final class CustomerTest extends TestCase
     /** @test **/
     public function create(): void
     {
-//        /** @var Customer $customer */
-//        $customer = EntityHelper::createFromXML(
-//            (string) file_get_contents(__DIR__ . '/../Data/Response/CreateCustomer.xml')
-//        );
-//
-//        var_dump($customer);
-//
-//        Assert::assertInstanceOf(Customer::class, $customer);
-//        Assert::assertSame($customer->getName(), 'Naam Klant');
-//        Assert::assertSame($customer->getStreet(), 'StraatNaam');
-//        Assert::assertSame($customer->getHouseNr(), 38);
-//        Assert::assertSame($customer->getHouseNrExtension(), '');
-//        Assert::assertSame($customer->getZipCode(), '1234AB');
-//        Assert::assertSame($customer->getCity(), 'Amsterdam');
-//        Assert::assertSame($customer->getCountryCode(), 'NLD');
-//        Assert::assertSame($customer->getPhone1(), '0612345678');
-//        Assert::assertSame($customer->getPhone2(), null);
-//        Assert::assertSame($customer->getFax(), null);
-//        Assert::assertSame($customer->getEmail(), 'klant@email.nl');
-//        Assert::assertSame($customer->getWebsite(), '');
-//        Assert::assertSame($customer->getDebitNr(), '');
-//        Assert::assertSame($customer->getIban(), null);
-//        Assert::assertSame($customer->getBic(), null);
-//        Assert::assertSame($customer->getVatNr(), null);
-//        Assert::assertSame($customer->getLegalStatus(), 'CV');
-//        Assert::assertSame($customer->getExternalId(), null);
-//        Assert::assertSame($customer->getChamberOfCommerceNr(), null);
-//        Assert::assertSame($customer->getCustomerId(), null);
-
-
-
-        $mockHandler = new MockHandler(
-            [new Response(200, [], (string) file_get_contents(__DIR__ . '/../Data/Response/CreateCustomer.xml'))]
-        );
-
-        $stack = HandlerStack::create($mockHandler);
-        $client = new OfficeClient('example.com', 'test', 'test', ['handler' => $stack]);
-
-
-        $client->webhook->process(
+        /** @var Customer $customer */
+        $customer = EntityHelper::createFromXML(
             (string) file_get_contents(__DIR__ . '/../Data/Response/CreateCustomer.xml')
         );
+
+        Assert::assertInstanceOf(Customer::class, $customer);
+        Assert::assertSame($customer->getName(), 'Naam Klant');
+        Assert::assertSame($customer->getStreet(), 'StraatNaam');
+        Assert::assertSame($customer->getHouseNr(), 38);
+        Assert::assertSame($customer->getHouseNrExtension(), null);
+        Assert::assertSame($customer->getZipCode(), '1234AB');
+        Assert::assertSame($customer->getCity(), 'Amsterdam');
+        Assert::assertSame($customer->getCountryCode(), 'NLD');
+        Assert::assertSame($customer->getPhone1(), '0612345678');
+        Assert::assertSame($customer->getPhone2(), null);
+        Assert::assertSame($customer->getFax(), null);
+        Assert::assertSame($customer->getEmail(), 'klant@email.nl');
+        Assert::assertSame($customer->getWebsite(), null);
+        Assert::assertSame($customer->getDebitNr(), null);
+        Assert::assertSame($customer->getIban(), null);
+        Assert::assertSame($customer->getBic(), null);
+        Assert::assertSame($customer->getVatNr(), null);
+        Assert::assertSame($customer->getLegalStatus(), 'CV');
+        Assert::assertSame($customer->getExternalId(), null);
+        Assert::assertSame($customer->getChamberOfCommerceNr(), null);
+        Assert::assertSame($customer->getCustomerId(), null);
     }
 //
 //    public function modify(): void
