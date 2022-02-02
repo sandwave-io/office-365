d2c0dffaec53b87c6f4fd681feed001eb47865e8
Willem Daems <willem@sandwave.io>
2022-02-02 15:14:48 +0100

    wip

--- tests/Integration/Component/CustomerTest.php
+++ tests/Integration/Component/CustomerTest.php
@@ -13,9 +13,9 @@ use SandwaveIo\Office365\Response\QueuedResponse;
 final class CustomerTest extends TestCase
 {
     /**
-     * @ test
+     * @test
      */
-    public function create(): void
+    public function customerCreate(): void
     {
         $mockHandler = new MockHandler(
             [new Response(200, [], (string) file_get_contents(__DIR__ . '/../Data/Response/NinaResponse_Success.xml'))]
@@ -24,11 +24,56 @@ final class CustomerTest extends TestCase
         $stack = HandlerStack::create($mockHandler);
         $officeClient = new OfficeClient('example.com', 'test', 'test', ['handler' => $stack]);
 
+        $officeClient = new OfficeClient('https://api.routit.nl', 'yourhosting_bv', 'm_wyd44MRbfEJLgqDiEgA');
+
         $customerResponse = $officeClient->customer->create(
-            'Naam Klant',
+            'Sandwave test',
             'StraatNaam',
             38,
             null,
+            '1234AB',
+            'Amsterdam',
+            'NLD',
+            '0612345678',
+            null,
+            null,
+            'klant@email.nl',
+            null,
+            null,
+            null,
+            null,
+            null,
+            'CV',
+            null,
+            null,
+            '134534659043869034809635435',
+        );
+
+        var_dump($customerResponse);
+
+        Assert::assertInstanceOf(QueuedResponse::class, $customerResponse);
+        Assert::assertTrue($customerResponse->isSuccess());
+        Assert::assertSame(0, $customerResponse->getErrorCode());
+    }
+
+    /**
+     * @test
+     */
+    public function customerModify(): void
+    {
+        $mockHandler = new MockHandler(
+            [new Response(200, [], (string) file_get_contents(__DIR__ . '/../Data/Response/NinaResponse_Success.xml'))]
+        );
+
+        $stack = HandlerStack::create($mockHandler);
+        $officeClient = new OfficeClient('example.com', 'test', 'test', ['handler' => $stack]);
+
+        $customerResponse = $officeClient->customer->modify(
+            'CID1322912',
+            'Naam',
+            'StraatNaam',
+            38,
+            '',
             '1234AB',
             'Amsterdam',
             'NLD',
@@ -52,10 +97,7 @@ final class CustomerTest extends TestCase
         Assert::assertSame(0, $customerResponse->getErrorCode());
     }
 
-    /**
-     * @test
-     */
-    public function modify(): void
+    public function customerUpdate(): void
     {
         $mockHandler = new MockHandler(
             [new Response(200, [], (string) file_get_contents(__DIR__ . '/../Data/Response/NinaResponse_Success.xml'))]
